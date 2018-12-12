39ac36103d3bc8d6abb8381c7f019f24ed6dd36e
crazy_fire <bolokadze@gmail.com>
2018-12-12 22:53:19 +0100

    + vegetables + UTs

--- src/test/java/garden/CommonTests.java
+++ src/test/java/garden/CommonTests.java
@@ -14,11 +14,13 @@ import java.util.Set;
 
 public class CommonTests {
 
+    /** We make sure that Generic types work */
     @Test
     public void testCollection() {
 
         final Set<Plant> myGarden = new LinkedHashSet<>();
 
+        // All vegetables are Plants, including all Tomatoes
         myGarden.add(new RedTomato(1.5));
         myGarden.add(new GreenTomato(1.01));
         myGarden.add(new Apple(1.3));
@@ -28,7 +30,11 @@ public class CommonTests {
         citruses.add(new Lemon(2.2));
         citruses.add(new Orange(1.4));
 
+        // This is Ok because Citrus extends AbstractFruit and AbstractFruit implements Plant
         myGarden.addAll(citruses);
+
+        //This shouldn't compile
+        //citruses.addAll(myGarden);
 
         for (Plant plant : myGarden) {
             System.out.println(plant);
